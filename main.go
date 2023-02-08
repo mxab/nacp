cfaf2a660fd7ee7f872fbcb8ba8b67f8bf6dc3a6
Max Fröhlich <maxbruchmann@gmail.com>
2023-02-08 23:35:19 +0100

    feat: detect create/update

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"regexp"
 )
 
 type NomadServer struct {
@@ -29,7 +30,18 @@ func NewServer(c Config) *httputil.ReverseProxy {
 	originalDirector := proxy.Director
 	proxy.Director = func(r *http.Request) {
 		originalDirector(r)
-		r.Header.Set("Some-Header", "Some Value")
+
+		// only check for the header if it is a POST request and path is /v1/jobs
+		if r.Method == "POST" && r.URL.Path == "/v1/jobs" {
+			r.Header.Set("NACP", "CREATE")
+		}
+
+		// only check for the header if it is a POST request
+		// and path matches a regex that starts with /v1/job/ followed by a reg job name (e.g. /v1/job/some-job)
+		if r.Method == "POST" && regexp.MustCompile("^/v1/job/.*").MatchString(r.URL.Path) {
+			r.Header.Set("NACP", "UPDATE")
+		}
+
 	}
 	return proxy
 }
--- main_test.go
+++ main_test.go
@@ -10,43 +10,65 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestProxy(t *testing.T) {
+// rewrite the test above as table driven test
+func TestProxyTableDriven(t *testing.T) {
 
-	nomadDummy := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		// Test request parameters
-
-		assert.Equal(t, req.Method, "POST")
-		assert.Equal(t, req.URL.String(), "/v1/jobs")
-
-		x := req.Header.Get("Some-Header")
-		assert.Equal(t, x, "Some Value", "Some-Header header has value 'Some Value'")
-		// Send response to be tested
-		rw.Write([]byte(`OK`))
-	}))
-	// Close the server when test finishes
-	defer nomadDummy.Close()
-
-	// Use Client & URL from our local test server
-	c := Config{
-		Port: 8080,
-		Bind: "0.0.0.0",
-		nomad: NomadServer{
-			Address: nomadDummy.URL,
+	type test struct {
+		name   string
+		path   string
+		method string
+		want   string
+	}
+	tests := []test{
+		{
+			name:   "create job",
+			path:   "/v1/jobs",
+			method: "POST",
+			want:   "CREATE",
+		},
+		{
+			name:   "update job",
+			path:   "/v1/job/some-job",
+			method: "POST",
+			want:   "UPDATE",
 		},
 	}
-	proxy := NewServer(c)
 
-	//http.Handle("/", proxy)
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			nomadDummy := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+				// Test request parameters
 
-	proxyServer := httptest.NewServer(proxy)
-	defer proxyServer.Close()
+				assert.Equal(t, req.Method, tc.method, "Ensure method is set to POST")
+				assert.Equal(t, req.URL.Path, tc.path, "Ensure path is set")
+				assert.Equal(t, req.Header.Get("NACP"), tc.want, "Ensure NACP header is set to CREATE")
 
-	res, err := http.Post(fmt.Sprintf("%s/v1/jobs", proxyServer.URL), "application/json", strings.NewReader(`
-	{}
-	`))
+				_, _ = rw.Write([]byte(`OK`))
+			}))
+			// Close the server when test finishes
+			defer nomadDummy.Close()
 
-	assert.NoError(t, err, "No http call error")
-	assert.Equal(t, res.StatusCode, 200, "OK response is expected")
-	// NACP header has value "hello"
+			// Use Client & URL from our local test server
+			c := Config{
+				Port: 8080,
+				Bind: "0.0.0.0",
+				nomad: NomadServer{
+					Address: nomadDummy.URL,
+				},
+			}
+			proxy := NewServer(c)
+
+			//http.Handle("/", proxy)
+
+			proxyServer := httptest.NewServer(proxy)
+			defer proxyServer.Close()
+
+			res, err := http.Post(fmt.Sprintf("%s%s", proxyServer.URL, tc.path), "application/json", strings.NewReader(`
+			{}
+			`))
+			assert.NoError(t, err, "No http call error")
+			assert.Equal(t, res.StatusCode, 200, "OK response is expected")
+		})
+	}
 
 }
