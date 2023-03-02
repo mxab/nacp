794ba1b0b546911bbd636eb8b8dfea06a3492263
Max Fröhlich <maxbruchmann@gmail.com>
2023-03-02 08:16:33 +0100

    chore: refactor test setup

--- main_test.go
+++ main_test.go
@@ -22,13 +22,222 @@ import (
 )
 
 // rewrite the test above as table driven test
-func TestProxyTableDriven(t *testing.T) {
+func TestProxy(t *testing.T) {
 
 	type test struct {
-		name                  string
-		path                  string
-		method                string
-		requestJson           string
+		name   string
+		path   string
+		method string
+
+		requestSender        func(*api.Client) (interface{}, *api.WriteMeta, error)
+		wantNomadRequestJson string
+		wantProxyResponse    interface{}
+		nomadResponse        string
+		//	responseWarnings []error
+		validators []admissionctrl.JobValidator
+		mutators   []admissionctrl.JobMutator
+	}
+
+	tests := []test{
+
+		{
+			name:   "create job adds hello meta",
+			path:   "/v1/jobs",
+			method: "PUT",
+
+			requestSender: func(c *api.Client) (interface{}, *api.WriteMeta, error) {
+				return c.Jobs().Register(testutil.ReadJob(t, "job.json"), nil)
+			},
+			wantNomadRequestJson: registerRequestJson(t, jobWithHelloWorldMeta(t)),
+
+			wantProxyResponse: &api.JobRegisterResponse{},
+
+			nomadResponse: toJson(t, &api.JobRegisterResponse{}),
+			validators:    []admissionctrl.JobValidator{},
+			mutators: []admissionctrl.JobMutator{
+				&mutator.HelloMutator{},
+			},
+		},
+
+		{
+			name:   "plan job adds hello meta",
+			path:   "/v1/job/example/plan",
+			method: "PUT",
+
+			requestSender: func(c *api.Client) (interface{}, *api.WriteMeta, error) {
+				return c.Jobs().Plan(testutil.ReadJob(t, "job.json"), false, nil)
+			},
+
+			wantNomadRequestJson: planRequestJson(t, jobWithHelloWorldMeta(t)),
+
+			wantProxyResponse: &api.JobPlanResponse{},
+
+			nomadResponse: toJson(t, &api.JobPlanResponse{}),
+
+			validators: []admissionctrl.JobValidator{},
+			mutators: []admissionctrl.JobMutator{
+				&mutator.HelloMutator{},
+			},
+		},
+		{
+			name:   "create job adds warnings",
+			path:   "/v1/jobs",
+			method: "PUT",
+
+			requestSender: func(c *api.Client) (interface{}, *api.WriteMeta, error) {
+				return c.Jobs().Register(testutil.ReadJob(t, "job.json"), nil)
+			},
+
+			wantNomadRequestJson: registerRequestJson(t, testutil.ReadJob(t, "job.json")),
+
+			wantProxyResponse: &api.JobRegisterResponse{
+				Warnings: "1 error occurred:\n\t* some warning\n\n",
+			},
+
+			nomadResponse: toJson(t, &api.JobRegisterResponse{}),
+			validators: []admissionctrl.JobValidator{
+				mockValidatorReturningWarnings("some warning"),
+			},
+			mutators: []admissionctrl.JobMutator{},
+		},
+		{
+			name:   "create job concats warnings",
+			path:   "/v1/jobs",
+			method: "PUT",
+
+			requestSender: func(c *api.Client) (interface{}, *api.WriteMeta, error) {
+				return c.Jobs().Register(testutil.ReadJob(t, "job.json"), nil)
+			},
+
+			wantNomadRequestJson: registerRequestJson(t, testutil.ReadJob(t, "job.json")),
+
+			wantProxyResponse: &api.JobRegisterResponse{
+				Warnings: "2 errors occurred:\n\t* 1 error occurred:\n\t* some warning\n\n\n\t* some warning\n\n",
+			},
+
+			nomadResponse: toJson(t, &api.JobRegisterResponse{
+				Warnings: multierror.Append(nil, fmt.Errorf("some warning")).Error(),
+			}),
+			validators: []admissionctrl.JobValidator{
+				mockValidatorReturningWarnings("some warning"),
+			},
+			mutators: []admissionctrl.JobMutator{},
+		},
+		{
+			name:   "plan job adds warnings",
+			path:   "/v1/job/example/plan",
+			method: "PUT",
+			requestSender: func(c *api.Client) (interface{}, *api.WriteMeta, error) {
+				return c.Jobs().Plan(testutil.ReadJob(t, "job.json"), false, nil)
+			},
+
+			wantNomadRequestJson: planRequestJson(t, testutil.ReadJob(t, "job.json")),
+
+			wantProxyResponse: &api.JobPlanResponse{
+				Warnings: "1 error occurred:\n\t* some warning\n\n",
+			},
+
+			nomadResponse: toJson(t, &api.JobPlanResponse{}),
+			validators: []admissionctrl.JobValidator{
+				mockValidatorReturningWarnings("some warning"),
+			},
+			mutators: []admissionctrl.JobMutator{},
+		},
+		// {
+		// 	name:   "validate job adds hello meta",
+		// 	path:   "/v1/validate/jobs",
+		// 	method: "PUT",
+
+		// 	requestSender: func(c *api.Client) (interface{}, *api.WriteMeta, error) {
+		// 		return c.Jobs().Validate(testutil.ReadJob(t, "job.json"), nil)
+		// 	},
+		// 	wantNomadRequestJson: toJson(t, &api.JobValidateRequest{Job: jobWithHelloWorldMeta(t)}),
+
+		// 	wantProxyResponse: &api.JobPlanResponse{},
+
+		// 	nomadResponse: toJson(t, &api.JobRegisterResponse{}),
+		// 	validators:    []admissionctrl.JobValidator{},
+		// 	mutators: []admissionctrl.JobMutator{
+		// 		&mutator.HelloMutator{},
+		// 	},
+		// },
+		//Validate
+		// {
+		// 	name:        "validate job adds warnings",
+		// 	path:        "/v1/validate/job",
+		// 	method:      "POST",
+		// 	requestJson: toJson(t, &api.JobValidateRequest{Job: &api.Job{}}),
+
+		// 	wantNomadRequestJson: toJson(t, &api.JobValidateRequest{Job: &api.Job{}}),
+		// 	wantProxyResponseJson: toJson(t, &api.JobValidateResponse{
+
+		// 		Warnings: "1 error occurred:\n\t* some warning\n\n",
+		// 	}),
+
+		// 	nomadResponse: toJson(t, &api.JobValidateResponse{}),
+		// 	validators: []admissionctrl.JobValidator{
+		// 		mockValidatorReturningWarnings("some warning"),
+		// 	},
+		// 	mutators: []admissionctrl.JobMutator{},
+		// },
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			nomadBackendCalled := false
+			nomadDummy := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+				// Test request parameters
+				nomadBackendCalled = true
+				assert.Equal(t, req.Method, tc.method, "Ensure method is set")
+				assert.Equal(t, req.URL.Path, tc.path, "Ensure path is set")
+				jsonData, err := io.ReadAll(req.Body)
+				if err != nil {
+					t.Fatal(err)
+				}
+				json := string(jsonData)
+				assert.JSONEq(t, tc.wantNomadRequestJson, json, "Body matches")
+
+				_, _ = rw.Write([]byte(tc.nomadResponse))
+			}))
+			// Close the server when test finishes
+			defer nomadDummy.Close()
+
+			// Use Client & URL from our local test server
+
+			nomad, err := url.Parse(nomadDummy.URL)
+			if err != nil {
+				t.Fatal(err)
+			}
+			jobHandler := admissionctrl.NewJobHandler(
+				tc.mutators,
+				tc.validators,
+				hclog.NewNullLogger(),
+			)
+			proxy := NewProxyHandler(nomad, jobHandler, hclog.NewNullLogger())
+
+			proxyServer := httptest.NewServer(http.HandlerFunc(proxy))
+			defer proxyServer.Close()
+			nomadClient := buildNomadClient(t, proxyServer)
+
+			resp, _, err := tc.requestSender(nomadClient)
+
+			assert.NoError(t, err, "No http call error")
+			assert.Equal(t, tc.wantProxyResponse, resp, "OK response is expected")
+
+			assert.True(t, nomadBackendCalled, "Nomad backend was called")
+
+		})
+	}
+
+}
+func TestJobUpdateProxy(t *testing.T) {
+
+	type test struct {
+		name        string
+		path        string
+		method      string
+		requestJson string
+
 		wantNomadRequestJson  string
 		wantProxyResponseJson string
 		nomadResponse         string
@@ -38,24 +247,10 @@ func TestProxyTableDriven(t *testing.T) {
 	}
 
 	tests := []test{
-		{
-			name:   "create job adds hello meta",
-			path:   "/v1/jobs",
-			method: "PUT",
 
-			requestJson:          registerRequestJson(t, testutil.ReadJob(t, "job.json")),
-			wantNomadRequestJson: registerRequestJson(t, jobWithHelloWorldMeta(t)),
-
-			wantProxyResponseJson: toJson(t, &api.JobRegisterResponse{}),
-			nomadResponse:         toJson(t, &api.JobRegisterResponse{}),
-			validators:            []admissionctrl.JobValidator{},
-			mutators: []admissionctrl.JobMutator{
-				&mutator.HelloMutator{},
-			},
-		},
 		{
-			name:        "update job adds hello meta",
-			path:        "/v1/job/some-job",
+			name:        "update job adds hello meta (does this method actually exist?)",
+			path:        "/v1/job/example",
 			method:      "PUT",
 			requestJson: registerRequestJson(t, testutil.ReadJob(t, "job.json")),
 
@@ -68,102 +263,14 @@ func TestProxyTableDriven(t *testing.T) {
 				&mutator.HelloMutator{},
 			},
 		},
-		{
-			name:                 "plan job adds hello meta",
-			path:                 "/v1/job/some-job/plan",
-			method:               "PUT",
-			requestJson:          planRequestJson(t, testutil.ReadJob(t, "job.json")),
-			wantNomadRequestJson: planRequestJson(t, jobWithHelloWorldMeta(t)),
-
-			wantProxyResponseJson: toJson(t, &api.JobRegisterResponse{}),
-			nomadResponse:         toJson(t, &api.JobRegisterResponse{}),
-
-			validators: []admissionctrl.JobValidator{},
-			mutators: []admissionctrl.JobMutator{
-				&mutator.HelloMutator{},
-			},
-		},
-		{
-			name:        "create job adds warnings",
-			path:        "/v1/jobs",
-			method:      "PUT",
-			requestJson: registerRequestJson(t, testutil.ReadJob(t, "job.json")),
-
-			wantNomadRequestJson: registerRequestJson(t, testutil.ReadJob(t, "job.json")),
-			wantProxyResponseJson: toJson(t, &api.JobRegisterResponse{
-				Warnings: "1 error occurred:\n\t* some warning\n\n",
-			}),
-
-			nomadResponse: toJson(t, &api.JobRegisterResponse{}),
-			validators: []admissionctrl.JobValidator{
-				mockValidatorReturningWarnings("some warning"),
-			},
-			mutators: []admissionctrl.JobMutator{},
-		},
-		{
-			name:        "create job concats warnings",
-			path:        "/v1/jobs",
-			method:      "PUT",
-			requestJson: registerRequestJson(t, testutil.ReadJob(t, "job.json")),
-
-			wantNomadRequestJson: registerRequestJson(t, testutil.ReadJob(t, "job.json")),
-			wantProxyResponseJson: toJson(t, &api.JobRegisterResponse{
-				Warnings: "2 errors occurred:\n\t* 1 error occurred:\n\t* some warning\n\n\n\t* some warning\n\n",
-			}),
-
-			nomadResponse: toJson(t, &api.JobRegisterResponse{
-				Warnings: multierror.Append(nil, fmt.Errorf("some warning")).Error(),
-			}),
-			validators: []admissionctrl.JobValidator{
-				mockValidatorReturningWarnings("some warning"),
-			},
-			mutators: []admissionctrl.JobMutator{},
-		},
-		{
-			name:        "plan job adds warnings",
-			path:        "/v1/job/some-job/plan",
-			method:      "PUT",
-			requestJson: planRequestJson(t, testutil.ReadJob(t, "job.json")),
-
-			wantNomadRequestJson: planRequestJson(t, testutil.ReadJob(t, "job.json")),
-			wantProxyResponseJson: toJson(t, &api.JobPlanResponse{
-				// Diff: &api.JobDiff{
-				// 	Fields: []*api.FieldDiff{
-				// 		{
-				// 			Name: "aaa",
-				// 			Type: "bbb",
-				// 			Old:  "ccc",
-				// 			New:  "ddd",
-				// 		},
-				// 	},
-				// },
-				Warnings: "1 error occurred:\n\t* some warning\n\n",
-			}),
-
-			nomadResponse: toJson(t, &api.JobPlanResponse{
-				// Diff: &api.JobDiff{
-				// 	Fields: []*api.FieldDiff{
-				// 		{
-				// 			Name: "aaa",
-				// 			Type: "bbb",
-				// 			Old:  "ccc",
-				// 			New:  "ddd",
-				// 		},
-				// 	},
-				// },
-			}),
-			validators: []admissionctrl.JobValidator{
-				mockValidatorReturningWarnings("some warning"),
-			},
-			mutators: []admissionctrl.JobMutator{},
-		},
 	}
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
+			nomadBackendCalled := false
 			nomadDummy := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 				// Test request parameters
-
+				nomadBackendCalled = true
 				assert.Equal(t, req.Method, tc.method, "Ensure method is set")
 				assert.Equal(t, req.URL.Path, tc.path, "Ensure path is set")
 				jsonData, err := io.ReadAll(req.Body)
@@ -198,9 +305,22 @@ func TestProxyTableDriven(t *testing.T) {
 			assert.NoError(t, err, "No http call error")
 			assert.Equal(t, 200, res.StatusCode, "OK response is expected")
 			assert.JSONEq(t, tc.wantProxyResponseJson, readClosterToString(t, res.Body), "Body matches")
+			assert.True(t, nomadBackendCalled, "Nomad backend was called")
+
 		})
 	}
 
+}
+
+func buildNomadClient(t *testing.T, proxyServer *httptest.Server) *api.Client {
+	t.Helper()
+	nomadClient, err := api.NewClient(&api.Config{
+		Address: proxyServer.URL,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	return nomadClient
 }
 func mockValidatorReturningWarnings(warning string) admissionctrl.JobValidator {
 
@@ -280,10 +400,9 @@ func TestAdmissionControllerErrors(t *testing.T) {
 	res, err := sendPut(t, fmt.Sprintf("%s%s", proxyServer.URL, "/v1/jobs"), strings.NewReader(jobRequestJson))
 	require.NoError(t, err, "No http call error")
 	assert.Equal(t, 500, res.StatusCode, "Should return 400")
-	body, err := io.ReadAll(res.Body)
+	_, err = io.ReadAll(res.Body)
 	require.NoError(t, err)
-	x := string(body)
-	t.Logf("response: %s", x)
+
 }
 
 func sendPut(t *testing.T, url string, body io.Reader) (*http.Response, error) {
